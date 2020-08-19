6a59edf43fd33497facf28b60cff75251871feb4
maniac@maniac <maniacdc>
2020-08-19 07:58:39 -0400

    remove NPM from the picture

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const childProcess = require('child_process')
-const spawned = childProcess.spawn('npm', ['run', 'tsc'], { stdio: 'pipe', shell: true, windowsHide: false })
+const spawned = childProcess.spawn('sh -c \'tsc -w\'', [], { stdio: 'pipe', shell: true, windowsHide: false })
 process.on('exit', () => { spawned.kill() })
